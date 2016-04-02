88caf863d3b5653ce925b2904259b815003a6db1
Dehyrf <Dehyrf@users.noreply.github.com>
2016-04-02 13:31:18 -0500

    Added status indicator light and fixed minor bug

--- waterMe/waterMe.ino
+++ waterMe/waterMe.ino
@@ -5,24 +5,26 @@
 //Initilize global variables
 #define ONE_SYNC_MILLIS (60 * 60 * 1000) //Define leeway for clock variation (is currently allowing 1 hour variation)
 unsigned long lastSync = millis();  //Timer from last time sync used later for getting realtime data
-int startPin = D6;  //Will start/stop the watering if connected/disconnected for a hardware failsafe
-bool enabled = true;//to hold startPin value
-int relayPin = D7;  //Pin turning pump control relay on/off and status can be seen on the board's small led
+int ledPin = D7;    //Pin for the led to indicate when relay is enabled
+int relayPin = D6;  //Pin turning pump control relay on/off
+int startPin = D5;  //Will start/stop the watering if connected/disconnected for a hardware failsafe
+bool enabled = false;//to hold startPin value
 int maxTime = (10 * 1000);   //Max time in seconds to run the pump per sitting
-int minDelay = (4 * 60 * 60 * 1000);    //Minimum amount of time in hours between watering sessions (to prevent water spillage)
+int minDelay = (4 * 60 * 60 * 1000);    //Minimum amount of time in hours between watering sessions (to prevent water spillage) 
 int autoDelay = (12 * 60 * 60 * 1000);  //period in hours between automated watering
 int canWater = 1;   //Tells user if they can water more today (1:yes, 0:no)
 int lastTime = 0;   //Time since last watering session (milliseconds)
 int totalWatered = 0;   //Times watered total;
 int lastAutoTime = 0;   //Time since last automated watering session
-int temp = 0;
+int temp = 0;   //Generic variable used for various tasks and previously for debugging
 
 // Register our Particle functions we will use that will allow for online control and setup io pins
 void setup()
 {
    //Start pin configuration
-   pinMode(startPin, INPUT);
+   pinMode(ledPin, OUTPUT);
    pinMode(relayPin, OUTPUT);
+   pinMode(startPin, INPUT);
    digitalWrite(relayPin, HIGH); //Ensure relay pin is low/off at the start
    
    if (Particle.connected() == false) { //Connect particle to cloud if it is not already
@@ -45,6 +47,15 @@ void loop()
     else
         canWater = 0;
         
+    if (canWater == 1 && temp > 7500) { //indicate watering status with the led
+        temp = 0;
+        digitalWrite(ledPin, HIGH);
+        delay(75);
+        digitalWrite(ledPin, LOW);
+    }
+    else if (canWater == 1)
+        temp++;
+        
     if (canWater == 1 && (millis() - lastAutoTime) > autoDelay) {   //Run auto water or delay another auto period
         lastAutoTime = millis();
         temp = remoteWater("water");
@@ -59,11 +70,15 @@ void loop()
 }
 
 int remoteWater(String in) {    //Water and reset time since last watering cycle
+    enabled = digitalRead(startPin);    //Read hardware enabling pin data
+    
     if (enabled && canWater == 1) {
         lastTime = millis();
         digitalWrite(relayPin, LOW);
+        digitalWrite(ledPin, HIGH);
         delay(maxTime);
         digitalWrite(relayPin, HIGH);
+        digitalWrite(ledPin, LOW);
         totalWatered++;
         return 1;
     }
